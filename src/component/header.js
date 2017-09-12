da00d40ab7303655fdb4ee2ea97821c9c14c6ddc
nelson torres <nelsondaniel88@gmail.com>
2017-09-12 00:32:43 -0400

    create the component header and some style

---
+++ src/component/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../style/style.css';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="title"><h1>The Tito Airline</h1></div>                
+            </header>
+        );
+    }
+}
